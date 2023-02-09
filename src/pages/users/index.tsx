c3dddbde287dd2cd6a582b6ec93d1f60b429d9bf
Pedro Ricardo Oliveira Córdova <pedroricardocordova@yahoo.com.br>
2023-02-09 16:17:49 -0300

    fea: end of userList

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "@chakra-ui/react";
 import {
   Box,
   Button,
@@ -23,19 +23,37 @@ import { Sidebar } from "../../components/sidebar";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { GrUpdate } from "react-icons/gr";
 
-import Link from "next/link";
-import { useUsers } from "../../services/hooks/useUsers";
+import NextLink from "next/link";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
 
-  const { data, isFetching, isLoading, error, refetch } = useUsers(page);
+  const { data, isFetching, isLoading, error, refetch } = useUsers(page, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ["user", userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 1000 * 60 * 10,
+      }
+    );
+  }
   return (
     <Box>
       <Header />
@@ -61,7 +79,7 @@ export default function UserList() {
                   onClick={() => refetch()}
                   leftIcon={<Icon as={GrUpdate} fontSize="17" color="white" />}
                 ></Button>
-                <Link href="/users/create" passHref>
+                <NextLink href="/users/create" passHref>
                   <Button
                     as="a"
                     size="sm"
@@ -71,7 +89,7 @@ export default function UserList() {
                   >
                     Criar novo
                   </Button>
-                </Link>
+                </NextLink>
               </Flex>
             )}
           </Flex>
@@ -105,7 +123,13 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            {/* {TODO: } */}
+                            <Link
+                              color="purple.400"
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                            >
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">
                               {user.email}
                             </Text>
@@ -142,3 +166,11 @@ export default function UserList() {
     </Box>
   );
 }
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+  return {
+    props: {
+      users,
+    },
+  };
+};
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,10 @@
-import { createServer, Factory, Model, Response } from "miragejs";
+import {
+  createServer,
+  Factory,
+  Model,
+  Response,
+  ActiveModelSerializer,
+} from "miragejs";
 import { faker } from "@faker-js/faker";
 
 type User = {
@@ -9,6 +15,9 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
     models: {
       user: Model.extend<Partial<User>>({}),
     },
@@ -43,8 +52,10 @@ export function makeServer() {
           pageStart,
           pageEnd
         );
+
         return new Response(200, { "x-total-count": String(total) }, { users });
       });
+      this.get("/users/:id");
       this.post("/users");
 
       this.namespace = "";
